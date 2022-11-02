f1c3b6d697fe0ecfaa3b2c48a78cd57296ec6b9a
AndSmi <developer@andsmi.ru>
2022-11-02 15:06:28 +0300

    Modify dist

--- vue.config.js
+++ vue.config.js
@@ -21,5 +21,5 @@ module.exports = defineConfig({
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
     types.forEach((type) => addStyleResources(config.module.rule('scss').oneOf(type)));
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/REPO_NAME/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/beauty-shop/' : '/',
 });
