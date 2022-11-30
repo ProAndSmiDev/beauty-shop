d24eee52d11984ba7e43e095562843059f829378
AndSmi <developer@andsmi.ru>
2022-11-30 08:41:38 +0300

    BS: Added services

---
+++ src/components/BeautyShopServices.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BeautyShopServices',
+});
+</script>
+
+<template>
+  <section class="bs-services">
+    <h2 class="bs-services__title">Services</h2>
+
+    <ul class="bs-services-list">
+      <li class="bs-services-list__item"></li>
+    </ul>
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/BeautyShopHome.vue
+++ src/views/BeautyShopHome.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { firstScreenHome } from '@/ts/constants/firstScreenHome';
+import BeautyShopServices from '@/components/BeautyShopServices.vue';
 import BeautyShopFirstScreen from '@/components/BeautyShopFirstScreen.vue';
 
 export default defineComponent({
   name: 'BeautyShopHome',
   components: {
+    BeautyShopServices,
     BeautyShopFirstScreen,
   },
   setup() {
@@ -27,6 +29,7 @@ export default defineComponent({
       :firstScreenInfo="firstScreenHome"
       class="bs-home__banner"
     />
+    <BeautyShopServices class="bs__services" />
   </div>
 </template>
 
